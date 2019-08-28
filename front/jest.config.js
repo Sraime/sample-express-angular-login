55da8df2673dbb3c33baa7983ec623f50b54109b
Unknown <robin.collas@hotmail.fr>
2019-08-28 17:18:31 +0200

    config sonar

--- front/jest.config.js
+++ front/jest.config.js
@@ -16,5 +16,8 @@ module.exports = {
     moduleNameMapper: {
       "\\.(jpg|jpeg|png)$": "<rootDir>/__mocks__/image.js",
       "^@lib/(.*)$": "<rootDir>/src/lib/$1"
-    }
+    },
+
+    testResultsProcessor: "jest-sonar-reporter",
+
 }
