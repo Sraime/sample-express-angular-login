9062d17e16644537357bdf89389324cac9c547ea
r.collas <robin.collas@hotmail.fr>
2019-08-29 17:01:35 +0200

    fix Blocker issue

--- back/app/tools/ValidationErrorParser/validation-error-parser.js
+++ back/app/tools/ValidationErrorParser/validation-error-parser.js
@@ -2,7 +2,7 @@ module.exports = {
     
     parseError: (err) => {
         if (!err.errors) return null;
-        errList = Object.entries(err.errors).map((i) => [i[0],[i[1].kind]]);
+        const errList = Object.entries(err.errors).map((i) => [i[0],[i[1].kind]]);
         return errList;
     }
 }
